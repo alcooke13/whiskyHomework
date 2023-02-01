93d421658a5fe871c39bca2708dcd7e05adf27df
effrancina <f.v.menichetti@gmail.com>
2023-02-01 16:15:37 +0000

    Initial commit

---
+++ src/test/java/com/codeclan/example/WhiskyTracker/WhiskyTrackerApplicationTests.java
@@ -0,0 +1,34 @@
+package com.codeclan.example.WhiskyTracker;
+
+import com.codeclan.example.WhiskyTracker.models.Distillery;
+import com.codeclan.example.WhiskyTracker.repositories.DistilleryRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@ActiveProfiles("test") //Indicates it's a test profile so will not run DataLoader
+@SpringBootTest
+public class WhiskyTrackerApplicationTests {
+
+	@Autowired
+	DistilleryRepository distilleryRepository;
+
+	@Test
+	public void contextLoads() {
+	}
+
+	@Test
+	public void canGetAllDistilleries(){
+		List<Distillery> foundDistil = distilleryRepository.findAll();
+		assertEquals(13, foundDistil.size());
+	}
+
+}
